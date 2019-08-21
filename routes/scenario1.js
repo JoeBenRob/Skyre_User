93bc0c5c4072b681b546cd4291b9ba5d2faf6513
JoeBenRob <JoeBenRob@gmail.com>
2019-08-21 10:30:36 +0100

    hook up

--- routes/scenario1.js
+++ routes/scenario1.js
@@ -3,11 +3,33 @@ const router = express.Router();
 const axios = require('axios');
 import passport from 'passport';
 
+router.get('/getBasicCitizens', (req, res, next) => {
 
-router.get('/getCitizens', (req, res, next) => {
-    console.log("---1 " + req.query.forenames);
     passport.authenticate('jwt', { session: false }, (err, user, info) => {
-        console.log("---1 " + req.query.forenames);
+        if (err) {
+            console.log(err);
+        }
+        if (info !== undefined) {
+            console.log(info.message);
+            console.log(info)
+            res.status(401).send(info.message);
+        } else {
+            let forenames = "forenames=" + req.query.forenames + "&";
+            let surname = "surname=" + req.query.surname;
+            let toSend = "" + forenames + surname;
+            axios.get(`http://52.137.47.18:8081/Citizen/getCitizens?${toSend}`)
+                .then(response => {
+                    res.json(response.data);
+                }).catch(err => {
+                    console.log(err);
+                });
+        }
+    })(req, res, next);
+});
+
+router.get('/getAdvCitizens', (req, res, next) => {
+
+    passport.authenticate('jwt', { session: false }, (err, user, info) => {
         if (err) {
             console.log(err);
         }
@@ -18,15 +40,14 @@ router.get('/getCitizens', (req, res, next) => {
         } else {
             let iforenames = "forenames=" + req.query.forenames + "&";
             let isurname = "surname=" + req.query.surname + "&";
-            // let icitizenId = "citizenId=" + req.query.citizenId + "&";
-            // let ihomeAddress = "homeAddress=" + req.query.homeAddress + "&";
-            // let idateOfBirth = "dateOfBirth=" + req.query.dateOfBirth + "&";
-            // let iplaceOfBirth = "placeOfBirth=" + req.query.placeOfBirth + "&";
-            // let isex = "sex=" + req.query.sex;
-            let appender = "" + iforenames + isurname;
-            console.log("-----3-----" + appender);
-            axios
-                .get(`http://51.144.152.19:8081/Citizen/getCitizens?${appender}`)
+            let icitizenId = "citizenId=" + req.query.citizenId + "&";
+            let ihomeAddress = "homeAddress=" + req.query.homeAddress + "&";
+            let idateOfBirth = "dateOfBirth=" + req.query.dateOfBirth + "&";
+            let iplaceOfBirth = "placeOfBirth=" + req.query.placeOfBirth + "&";
+            let isex = "sex=" + req.query.sex;
+            let appender = "" + iforenames + isurname + icitizenId + ihomeAddress + idateOfBirth + iplaceOfBirth + isex;
+            console.log(appender);
+            axios.get(`http://52.137.47.18:8081/Citizen/getCitizens?${appender}`)
                 .then(response => {
                     res.json(response.data);
                 }).catch(err => {
--- server.js
+++ server.js
@@ -8,12 +8,10 @@ import swaggerUi from 'swagger-ui-express';
 import passport from 'passport';
 
 const scenario1 = require('./routes/scenario1');
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 const API_PORT = process.env.API_PORT || 9003;
 
@@ -42,33 +40,28 @@ const options = {
 
 const swaggerSpec = swaggerJSDoc(options);
 
-// app.get('/swagger.json', (req, res) => {
-//   res.setHeader('Content-Type', 'application/json');
-//   res.send(swaggerSpec);
-// });
+app.get('/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
 
 require('./config/passport');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(Cors());
-
 app.use(logger('dev'));
 app.use(passport.initialize());
 
-
 app.use("/scenario1", scenario1);
-
 
 require('./routes/loginUser')(app);
 require('./routes/registerUser')(app);
 require('./routes/forgotPassword')(app);
 require('./routes/resetPassword')(app);
 require('./routes/updatePassword')(app);
-require('./routes/updatePasswordViaEmail')(app);
 require('./routes/findUsers')(app);
 require('./routes/deleteUser')(app);
 require('./routes/updateUser')(app);
-
 
 app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
 
